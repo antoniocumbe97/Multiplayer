fc5037b9080482c0da6d1768fb63465743a0a5c8
António Luís Cumbe <56931285+antoniocumbe97@users.noreply.github.com>
2021-10-02 23:07:40 +0200

    api das disciplinas

---
+++ public/api/subject.js
@@ -0,0 +1,30 @@
+let subjects = [
+    {
+        "categoria":"biologia",
+        "valor":"Biologia"
+    },{
+        "categoria":"geral",
+        "valor":"Cultura-Geral"
+    },{
+        "categoria":"Física",
+        "valor":"Física"
+    },{
+        "categoria":"historia",
+        "valor":"História"
+    },{
+        "categoria":"ingles",
+        "valor":"Inglês"
+    },{
+        "categoria":"matematica",
+        "valor":"Matemática"
+    },{
+        "categoria":"portugues",
+        "valor":"Português"
+    },{
+        "categoria":"quimica",
+        "valor":"Química"
+    }
+];
+export default function getSubjects() {
+    return subjects;
+}
